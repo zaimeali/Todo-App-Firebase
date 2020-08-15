401dc0398d79277590920aebc8563e4e7945d745
abdul-raffay <abdul.raffay@nytrotech.com>
2020-08-15 17:11:19 +0500

    Left

--- app.js
+++ app.js
@@ -1,12 +1,89 @@
-function popup() {
-    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center;';
+// POP-UPS
+function complete_task_popup() {
+    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center; background-color: rgb(81, 190, 81);';
     document.getElementById('popup').innerHTML = "Well Done🔥💯";
     setTimeout(() => {
         document.getElementById('popup').style.display = 'none';
-    }, 1500);
+    }, 2000);
 }
 
-function handleChange({ target }) {
-    popup();
+function new_task_popup() {
+    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center; background-color: rgb(57, 121, 206);';
+    document.getElementById('popup').innerHTML = "New Task Added🌑🚩";
+    setTimeout(() => {
+        document.getElementById('popup').style.display = 'none';
+    }, 2000);
+}
+
+function undo_task_popup() {
+    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center; background-color: rgb(206, 57, 57);';
+    document.getElementById('popup').innerHTML = "Oho! Again Added🤦😟";
+    setTimeout(() => {
+        document.getElementById('popup').style.display = 'none';
+    }, 2000);
+}
+
+// Variables
+let todoList = [];
+
+// ToDo App Functionality
+function windowLoad() {
+    getFirebaseData();
+}
+
+function refreshTodo() {
+    document.getElementById('toDo').innerHTML = '';
+    let myList = document.getElementById('toDo');
+
+    todoList.map(({id, task, isComplete}) => {
+        let list = document.createElement("li");
+        let clickable = document.createElement("button");
+        clickable.innerHTML = task;
+        clickable.id = id;
+        clickable.className = "btn";
+        clickable.setAttribute("onclick", `updateTodo('${id}', '${task}', ${isComplete})`);
+        list.appendChild(clickable);
+        list.className = `list_task ${isComplete ? "done" : "notDone"}`;
+        myList.appendChild(list);
+    });
+}
+
+function updateTodo(id, task, isComplete) {
+    document.getElementById('toDo').innerHTML = '';
+    firebase.database().ref('todo/' + id).set({
+        id: id,
+        isComplete: !isComplete,
+        task: task
+    });
+    getFirebaseData();
+}
+
+function removeAllTasks() {
+    firebase.database().ref('todo').remove();
+    console.log("All Tasks are Removed Successfully");
+    // console.log(todoList);
+}
+
+function createTodo({ target }) {
+    document.getElementById('toDo').innerHTML = '';
+    let key = firebase.database().ref('todo').push().key;
+    let new_task = {
+        id: key,
+        task: target.value,
+        isComplete: false,
+    }
+    // firebase.database().ref('todo/' + key).set(new_task);
+    firebase.database().ref('todo/' + key).set(new_task);
+    new_task_popup();
     target.value = '';
+    // getFirebaseData();
+}
+
+function getFirebaseData() {
+    document.getElementById('toDo').innerHTML = '';
+    firebase.database().ref('todo').on('child_added', function(data) {
+        // console.log(data.val());
+        todoList = [...todoList, data.val()];
+        refreshTodo();
+    });
 }
---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAoTB-DJNz9DWTZ6V4z1X_dXcCUHzpXeZI",
+    authDomain: "my-todo-app-f6455.firebaseapp.com",
+    databaseURL: "https://my-todo-app-f6455.firebaseio.com",
+    projectId: "my-todo-app-f6455",
+    storageBucket: "my-todo-app-f6455.appspot.com",
+    messagingSenderId: "798935006363",
+    appId: "1:798935006363:web:c97316f396b77432c85a3e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
