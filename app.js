c013ba14925415bff8a63178e5774d00670e18ff
abdul-raffay <abdul.raffay@nytrotech.com>
2020-08-15 22:37:00 +0500

    Sesky Logix

--- app.js
+++ app.js
@@ -23,67 +23,102 @@ function undo_task_popup() {
     }, 2000);
 }
 
+function remove_all_tasks_popup() {
+    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center; background-color: rgb(57, 121, 206);';
+    document.getElementById('popup').innerHTML = "All Tasks are Removed🙂🌼";
+    setTimeout(() => {
+        document.getElementById('popup').style.display = 'none';
+    }, 2000);
+}
+
 // Variables
 let todoList = [];
 
 // ToDo App Functionality
 function windowLoad() {
-    getFirebaseData();
+    // getFirebaseData();
+    refreshTodo();
 }
 
-function refreshTodo() {
-    document.getElementById('toDo').innerHTML = '';
-    let myList = document.getElementById('toDo');
+document.getElementById('toDo').innerHTML = '';
+let myList = document.getElementById('toDo');
 
-    todoList.map(({id, task, isComplete}) => {
-        let list = document.createElement("li");
-        let clickable = document.createElement("button");
-        clickable.innerHTML = task;
-        clickable.id = id;
-        clickable.className = "btn";
-        clickable.setAttribute("onclick", `updateTodo('${id}', '${task}', ${isComplete})`);
-        list.appendChild(clickable);
-        list.className = `list_task ${isComplete ? "done" : "notDone"}`;
-        myList.appendChild(list);
+function refreshTodo() {
+    firebase.database().ref('todo').on('child_added', function(data){
+        // console.log(data.val());
+        todoList = [data.val()];
+        // console.log(data.val());
+        // console.log(todoList);
+        todoList.map(({ id, task, isComplete }) => {
+            let list = document.createElement("li");
+            let clickable = document.createElement("button");
+            clickable.innerHTML = task;
+            clickable.id = id;
+            clickable.className = "btn";
+            // clickable.setAttribute("onclick", `updateTodo('${id}', '${task}', ${isComplete})`);
+            list.appendChild(clickable);
+            list.className = `list_task ${isComplete ? "done" : "notDone"}`;
+            myList.appendChild(list);
+        });
     });
+    // document.getElementById('toDo').innerHTML = '';
+    // let myList = document.getElementById('toDo');
+    // getFirebaseData();
+    // console.log(todoList);
+    // todoList.map(({id, task, isComplete}) => {
+    //     let list = document.createElement("li");
+    //     let clickable = document.createElement("button");
+    //     clickable.innerHTML = task;
+    //     clickable.id = id;
+    //     clickable.className = "btn";
+    //     clickable.setAttribute("onclick", `updateTodo('${id}', '${task}', ${isComplete})`);
+    //     list.appendChild(clickable);
+    //     list.className = `list_task ${isComplete ? "done" : "notDone"}`;
+    //     myList.appendChild(list);
+    // });
 }
 
 function updateTodo(id, task, isComplete) {
-    document.getElementById('toDo').innerHTML = '';
-    firebase.database().ref('todo/' + id).set({
-        id: id,
-        isComplete: !isComplete,
-        task: task
-    });
-    getFirebaseData();
+    // document.getElementById('toDo').innerHTML = '';
+    // firebase.database().ref('todo/' + id).set({
+    //     id: id,
+    //     isComplete: !isComplete,
+    //     task: task
+    // });
+    // todoList = [];
+    // getFirebaseData();
 }
 
 function removeAllTasks() {
     firebase.database().ref('todo').remove();
     console.log("All Tasks are Removed Successfully");
+    todoList = [];
     // console.log(todoList);
+    document.getElementById('toDo').innerHTML = '';
+    remove_all_tasks_popup();
+    // refreshTodo();
 }
 
 function createTodo({ target }) {
-    document.getElementById('toDo').innerHTML = '';
+    // document.getElementById('toDo').innerHTML = '';
     let key = firebase.database().ref('todo').push().key;
     let new_task = {
         id: key,
         task: target.value,
         isComplete: false,
     }
-    // firebase.database().ref('todo/' + key).set(new_task);
     firebase.database().ref('todo/' + key).set(new_task);
     new_task_popup();
     target.value = '';
+    // refreshTodo();
     // getFirebaseData();
 }
 
 function getFirebaseData() {
-    document.getElementById('toDo').innerHTML = '';
-    firebase.database().ref('todo').on('child_added', function(data) {
-        // console.log(data.val());
-        todoList = [...todoList, data.val()];
-        refreshTodo();
-    });
+    // document.getElementById('toDo').innerHTML = '';
+    // firebase.database().ref('todo').once('value', function(data) {
+    //     // console.log(data.val());
+    //     todoList = [data.val()];
+    //     // refreshTodo();
+    // });
 }
